dbf0ad534fbc67e0814acdd34979c66cd9770a28
micklongen@163.com <micklongen@163.com>
2023-05-21 23:33:31 -0700

    feat::完善 回归 代码

--- 02. 人工智能算法/1. 机器学习/监督学习-回归/Regression1/regression.py
+++ 02. 人工智能算法/1. 机器学习/监督学习-回归/Regression1/regression.py
@@ -1,6 +1,3 @@
-"""
-    对ex1data1.txt中的数据进行线性回归，部分样本用来训练，部分样本用来预测
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
@@ -11,34 +8,35 @@ plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 # 数据格式：城市人口,食品经销商利润
 
-# 读取数据
-data = np.loadtxt('data.txt', delimiter=',')
-data_X = data[:, 0]
-data_y = data[:, 1]
+if __name__=='__main__':
+    # 读取数据
+    data = np.loadtxt('data.txt', delimiter=',')
+    data_X = data[:, 0]
+    data_y = data[:, 1]
 
-# 数据分割
-X_train, X_test, y_train, y_test = train_test_split(data_X, data_y)
+    # 数据分割
+    X_train, X_test, y_train, y_test = train_test_split(data_X, data_y)
 
-# 训练模型
-model = LinearRegression()
-model.fit(X_train.reshape([-1, 1]), y_train)
+    # 训练模型
+    model = LinearRegression()
+    model.fit(X_train.reshape([-1, 1]), y_train)
 
-# 利用模型进行预测
-y_predict = model.predict(X_test.reshape([-1, 1]))
+    # 利用模型进行预测
+    y_predict = model.predict(X_test.reshape([-1, 1]))
 
-# 结果可视化
-plt.scatter(X_test, y_test, color='red')  # 测试样本
-plt.plot(X_test, y_predict, color='blue', linewidth=3)
-plt.xlabel('城市人口')
-plt.ylabel('食品经销商利润')
-plt.title('线性回归——城市人口与食品经销商利润的关系')
-plt.show()
+    # 结果可视化
+    plt.scatter(X_test, y_test, color='red')  # 测试样本
+    plt.plot(X_test, y_predict, color='blue', linewidth=3)
+    plt.xlabel('城市人口')
+    plt.ylabel('食品经销商利润')
+    plt.title('线性回归——城市人口与食品经销商利润的关系')
+    plt.show()
 
-# 模型参数
-print(model.coef_)
-print(model.intercept_)
-# MSE
-print(mean_squared_error(y_test, y_predict))
-# R^2
-print(r2_score(y_test, y_predict))
+    # 模型参数
+    print(model.coef_)
+    print(model.intercept_)
+    # MSE
+    print(mean_squared_error(y_test, y_predict))
+    # R^2
+    print(r2_score(y_test, y_predict))
 
--- 02. 人工智能算法/1. 机器学习/监督学习-回归/Regression2/regression.py
+++ 02. 人工智能算法/1. 机器学习/监督学习-回归/Regression2/regression.py
@@ -1,9 +1,3 @@
-#! /usr/bin/env python
-# coding:utf-8
-
-"""
-    对ex1data2.txt中的数据进行线性回归，所有样本都用来训练和预测
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
@@ -14,33 +8,34 @@ plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 # 数据格式：城市人口,房间数目,房价
 
-# 读取数据
-data = np.loadtxt('data.txt', delimiter=',')
-data_X = data[:, 0:2]
-data_y = data[:, 2]
+if __name__=='__main__':
+    # 读取数据
+    data = np.loadtxt('data.txt', delimiter=',')
+    data_X = data[:, 0:2]
+    data_y = data[:, 2]
 
-# 训练模型
-model = LinearRegression()
-model.fit(data_X, data_y)
+    # 训练模型
+    model = LinearRegression()
+    model.fit(data_X, data_y)
 
-# 利用模型进行预测
-y_predict = model.predict(data_X)
+    # 利用模型进行预测
+    y_predict = model.predict(data_X)
 
-# 结果可视化
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.scatter(data_X[:, 0], data_X[:, 1], data_y, color='red')
-ax.plot(data_X[:, 0], data_X[:, 1], y_predict, color='blue')
-ax.set_xlabel('城市人口')
-ax.set_ylabel('房间数目')
-ax.set_zlabel('房价')
-plt.title('线性回归——城市人口、房间数目与房价的关系')
-plt.show()
+    # 结果可视化
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.scatter(data_X[:, 0], data_X[:, 1], data_y, color='red')
+    ax.plot(data_X[:, 0], data_X[:, 1], y_predict, color='blue')
+    ax.set_xlabel('城市人口')
+    ax.set_ylabel('房间数目')
+    ax.set_zlabel('房价')
+    plt.title('线性回归——城市人口、房间数目与房价的关系')
+    plt.show()
 
-# 模型参数
-print(model.coef_)
-print(model.intercept_)
-# MSE
-print(mean_squared_error(data_y, y_predict))
-# R^2
-print(r2_score(data_y, y_predict))
+    # 模型参数
+    print(model.coef_)
+    print(model.intercept_)
+    # MSE
+    print(mean_squared_error(data_y, y_predict))
+    # R^2
+    print(r2_score(data_y, y_predict))
